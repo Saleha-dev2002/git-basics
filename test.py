80b21c225b2655a20c2c7f764d93cd14096b9e4b
Saleha-dev2002 <salehashahbaz197@gmail.com>
2026-07-13 03:57:01 -0700

    lets check

---
+++ test.py
@@ -0,0 +1,10 @@
+# Aapka pehla proper Python program!
+naam = "Saleha"
+umar = 24
+shauq = "Coding aur Data Science"
+
+print("--- My Profile ---")
+print("Assalam-o-Alaikum, Mera naam " + naam + " hai.")
+print("Meri umar " + str(umar) + " saal hai.")
+print("Mujhe " + shauq + " ka bht shauq hai!")
+
