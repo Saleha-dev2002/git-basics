034a7120887dd660be64274391a5843b3eb1f758
Saleha-dev2002 <salehashahbaz197@gmail.com>
2026-07-14 04:29:17 -0700

    Refactored user profile script using modern f-strings

--- test.py
+++ test.py
@@ -1,10 +1,11 @@
-# Aapka pehla proper Python program!
-naam = "Saleha"
-umar = 24
-shauq = "Coding aur Data Science"
+# User Profile Generator using Python f-strings
+# 1. Defining User Attributes (Variables)
+user_name = "Saleha"
+user_age = 22
+user_interest = "Coding and Data Science"
 
-print("--- My Profile ---")
-print("Assalam-o-Alaikum, Mera naam " + naam + " hai.")
-print("Meri umar " + str(umar) + " saal hai.")
-print("Mujhe " + shauq + " ka bht shauq hai!")
-
+# 2. Displaying the Profile (Using modern f-strings)
+print("-- User Profile --")
+print(f"Hello, my name is {user_name}.")
+print(f"I am {user_age} year old. ")
+print(f"I am highly passionate about {user_interest}!")
